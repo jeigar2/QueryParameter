6b4d324d5670c248411f4db3a1bb2a31e54fd2a6
Jesus García <jeigar2@gmail.com>
2024-03-07 21:41:09 +0100

    version 0.2 Tiene un textarea que recibe las trazas y se deben mostrar en el segundo textarea, la query con los parametro reemplazados por sus valores, se debe poder copiar la salida al portapapeles

--- src/main/java/es/comepiedras/QueryParameters.java
+++ src/main/java/es/comepiedras/QueryParameters.java
@@ -2,9 +2,12 @@ package es.comepiedras;
 
 import es.comepiedras.config.AppConfing;
 import io.javalin.Javalin;
+//import org.json.JSONArray;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.List;
 
 public class QueryParameters {
     public static void main(String[] args) {
@@ -13,17 +16,32 @@ public class QueryParameters {
         // Configurar ruta para servir la página HTML
         //app.get("/", ctx -> ctx.render("/index.html")); Esto está dando error, así que lo comento
 
-        // Configurar ruta para manejar la solicitud de la query y los argumentos
+        // Configurar ruta para manejar la solicitud de la query
         app.post("/query", ctx -> {
-            // Obtener la query y los argumentos del cuerpo de la solicitud
-            String query = ctx.formParam("query");
-            String arguments = ctx.formParam("arguments");
+            // Obtener la query del cuerpo de la solicitud
+            String content = ctx.formParam("content");
 
-            // Crear la query con los argumentos reemplazados
-            String queryWithArguments = QueryParameters.reemplazarSQLConParametros( query, arguments);
+//            JSONArray queries = new JSONArray();
+            String[] lines = content.split("\n");
+            String currentQuery = null;
+            List<String> queriesWithParams = new ArrayList<>();
+
+            for (String line : lines) {
+                if (line.contains("Preparing:")) {
+                    // Guardar la query actual
+                    currentQuery = line;//line.substring(11); // Eliminamos "Preparing:" del inicio de la línea
+                } else if (line.contains("Parameters:")) {
+                    // Si encontramos "Parameters:", esta línea son los parámetros de la query anterior
+                    if (currentQuery != null) {
+                        String params = line; //line.substring(11); // Eliminamos "Parameters:" del inicio de la línea
+                        // Crear la query con los argumentos reemplazados
+                        queriesWithParams.add(QueryParameters.reemplazarSQLConParametros( currentQuery, params));
+                    }
+                }
+            }
 
             // Enviar la query con los argumentos reemplazados como respuesta
-            ctx.result(queryWithArguments);
+            ctx.result(String.join("\n", queriesWithParams));
         });
 
     }
--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,6 +1,5 @@
 function buildQuery() {
-    var query = document.getElementById("query").value;
-    var arguments = document.getElementById("arguments").value;
+    var content = document.getElementById("content").value;
 
     // Hacer la solicitud POST al servidor
     fetch('/query', {
@@ -9,8 +8,7 @@ function buildQuery() {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: new URLSearchParams({
-            query: query,
-            arguments: arguments
+            "content": content
         })
     })
     .then(response => response.text())
@@ -21,8 +19,7 @@ function buildQuery() {
 }
 
 function clearFields() {
-    document.getElementById("query").value = "";
-    document.getElementById("arguments").value = "";
+    document.getElementById("content").value = "";
     document.getElementById("result").value = "";
 }
 
@@ -30,8 +27,7 @@ function clearFields() {
 function setWidth() {
     var screenWidth = window.innerWidth;
     var textareaWidth = Math.floor(screenWidth * 0.9 / 8); // Asumiendo un tamaño de caracter promedio
-    document.getElementById("query").cols = Math.max(50, textareaWidth); // Establece un mínimo de 50 columnas
-    document.getElementById("arguments").cols = Math.max(50, textareaWidth);
+    document.getElementById("content").cols = Math.max(50, textareaWidth); // Establece un mínimo de 50 columnas
     document.getElementById("result").cols = Math.max(50, textareaWidth);
 }
 
@@ -39,10 +35,21 @@ function copyOutputToClipboard() {
     var outputTextarea = document.getElementById("result");
     outputTextarea.select();
     document.execCommand("copy");
-    alert("Salida copiada al portapapeles: " + outputTextarea.value);
-
+    //alert("Salida copiada al portapapeles: " + outputTextarea.value);
+}
 // Evento que se dispara cuando se redimensiona la pantalla
 window.addEventListener("resize", setWidth);
+// Eventos de clic para los botones
+document.getElementById("buildButton").addEventListener("click", buildQuery);
+document.getElementById("clearButton").addEventListener("click", clearFields);
 
 setWidth();
 
+/*
+2023-10-27 12:24:10,203 DEBUG IFacturaATRPdPDAO.findFicherosBasePdPDTO ooo Using Connection [org.jboss.jca.adapters.jdbc.jdk8.WrappedConnectionJDK8@62b30f94]
+2023-10-27 12:24:10,203 DEBUG IFacturaATRPdPDAO.findFicherosBasePdPDTO ooo Using Connection [org.jboss.jca.adapters.jdbc.jdk8.WrappedConnectionJDK8@62b30f94]
+2023-10-27 12:24:10,204 DEBUG IFacturaATRPdPDAO.findFicherosBasePdPDTO ==>  Preparing: SELECT FAC.X_FACTURA_ATR_HUELLA, FAC.PUN_X_CUPS_20, FAC.D_CODIGO_FISCAL, FAC.F_FECHA_FACTURA, KGT.FIC_X_NOMBRE_FICHERO AS NOMBRE_FICHERO_ZIP, KGT.D_NOMBRE_FICHERO_PLANO AS NOMBRE_FICHERO_PLANO FROM V_SAT_FACTURAS_ATR FAC LEFT JOIN KGT_FICHEROS_BLOQUES KGT ON FAC.X_FACTURA_ATR_HUELLA = KGT.X_HUELLA WHERE FAC.D_CODIGO_FISCAL IN ( ? ) AND FAC.F_FECHA_FACTURA IN ( ? ) AND FAC.LINEG_X_LINEA_NEGOCIO=? AND FAC.PAI_X_PAIS=? AND FAC.X_FACTURA_ATR_HUELLA IN ( ? )
+2023-10-27 12:24:10,204 DEBUG IFacturaATRPdPDAO.findFicherosBasePdPDTO ==> Parameters: 19070117440(String), 15/07/2019(String), 2(Long), 2(Long), 2306061014267641601000000110267(BigDecimal)
+2023-10-27 12:24:10,250 TRACE IFacturaATRPdPDAO.findFicherosBasePdPDTO <==    Columns: X_FACTURA_ATR_HUELLA, PUN_X_CUPS_20, D_CODIGO_FISCAL, F_FECHA_FACTURA, NOMBRE_FICHERO_ZIP, NOMBRE_FICHERO_PLANO
+2023-10-27 12:24:10,250 TRACE IFacturaATRPdPDAO.findFicherosBasePdPDTO <==    Columns: X_FACTURA_ATR_HUELLA, PUN_X_CUPS_20, D_CODIGO_FISCAL, F_FECHA_FACTURA, NOMBRE_FICHERO_ZIP, NOMBRE_FICHERO_PLANO
+*/
